977d1361e9b124596ecccf280215448e254b0a92
HiroUrata <1712026@i.softbank.jp>
2021-10-20 22:29:42 +0900

    Qiita投稿用

--- EnumLessonQText/ViewController.swift
+++ EnumLessonQText/ViewController.swift
@@ -9,9 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    enum UIKit:String{
+        case UIButton = "ボタンです"
+        case UILabel = "ラベルです"
+        case UITextField = "テキストフィールドです"
+        case UITextView = "テキストビューです"
+        case UITableView = "テーブルビューです"
+        case UICollectionView = "コレクションビューです"
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let label = {() -> UILabel in
+
+            let uiLabel = UILabel(frame: CGRect(x: view.frame.maxX / 4, y: 100, width: view.frame.width / 4, height: 50))
+            uiLabel.text = UIKit.UICollectionView.rawValue
+            
+            return uiLabel
+        }()
+        view.addSubview(label)
+       
+        print(UIKit.UIButton)
+        print(UIKit.UICollectionView)
+        
     }
 
 
